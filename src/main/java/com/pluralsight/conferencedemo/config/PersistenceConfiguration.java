6d103ba56e207b8ce040c7ccc40683d7c78b740c
Bilal Tariq <bilaltariq690@gmail.com>
2020-01-21 15:22:57 +0000

    commit for deploy

---
+++ src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
@@ -0,0 +1,18 @@
+package com.pluralsight.conferencedemo.config;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class PersistenceConfiguration {
+    @Bean
+    public DataSource datasource() {
+        DataSourceBuilder builder = DataSourceBuilder.create();
+        builder.url("jdbc:postgresql://localhost:5432/conference-demo");
+        System.out.println("My custom DataSoource Bean has been initalized");
+        return builder.build();
+    }
+}
