0d4f997dba613de95c2e3fe0429de8f3cf13983e
Bilal Tariq <bilaltariq690@gmail.com>
2020-01-21 15:29:28 +0000

    Removing hard coded override bean

--- src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
+++ src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
@@ -8,11 +8,11 @@ import javax.sql.DataSource;
 
 @Configuration
 public class PersistenceConfiguration {
-    @Bean
-    public DataSource datasource() {
-        DataSourceBuilder builder = DataSourceBuilder.create();
-        builder.url("jdbc:postgresql://localhost:5432/conference-demo");
-        System.out.println("My custom DataSoource Bean has been initalized");
-        return builder.build();
-    }
+//    @Bean
+//    public DataSource datasource() {
+//        DataSourceBuilder builder = DataSourceBuilder.create();
+//        builder.url("jdbc:postgresql://localhost:5432/conference-demo");
+//        System.out.println("My custom DataSoource Bean has been initalized");
+//        return builder.build();
+//    }
 }
